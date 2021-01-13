77fa8efe633f7a8bd670b88f7f81a05acbfb1224
ObikaKelvin <obikakelvin.2255@gmail.com>
2021-01-13 07:29:43 +0100

    created a new home template and moved the templating logic to the index file

--- includes/frontend/templates/home.php
+++ includes/frontend/templates/home.php
@@ -1,10 +1,46 @@
-
-    <h2 class="ww-heading-primary">
-        <?php echo sprintf( wordpress_webhooks()->helpers->translate( 'Welcome to %s', 'ww-page-actions' ), WW_NAME ); ?>
-    </h2>
-
-    <div>
-    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+<div class="d-flex flex-column align-items-center">
+    <div class="ww_home--heading">
+        <h2 class="ww-heading-primary">
+            <?php echo sprintf( wordpress_webhooks()->helpers->translate( 'Welcome to %s', 'ww-page-actions' ), WW_NAME ); ?>
+        </h2>
     </div>
+
+    <p class="ww_home--text">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores</p>
+
+    <div class="ww_info d-flex">
+        <div class="card ww_card">
+            <div class="card-header">GET STARTED</div>
+            <div class="card-body">
+                <p class="ww_card--text">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero</p>
+            </div>
+            <a href="?page=wordpress_webhooks&tab=settings" class="btn btn-primary ww_btn">Set Up</a>
+        </div>
+
+        <div class="card ww_card">
+            <div class="card-header">GO PRO</div>
+            <div class="card-body">
+                <p class="ww_card--text">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero</p>
+            </div>
+            <a href="#" class="btn btn-primary ww_btn">Upgrade Plugin</a>
+        </div>
+
+        <div class="card ww_card">
+            <div class="card-header">CONTACT US</div>
+            <div class="card-body">
+                <p class="ww_card--text">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero</p>
+            </div>
+            <a href="#" class="btn btn-primary ww_btn">Contact Us</a>
+        </div>
+    </div>
+
+    <div class="ww_plugin--rate">
+        <div class="card ww_card">
+            <div class="card-header">Please us spread the word and keep this plugin up to date</div>
+            <div class="card-body">
+                <p class="ww_card--text">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero</p>
+            </div>
+        </div>
+    </div>
+</div>
 
 
